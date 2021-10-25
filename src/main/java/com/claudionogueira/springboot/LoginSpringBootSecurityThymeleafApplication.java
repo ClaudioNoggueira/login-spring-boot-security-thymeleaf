cfcbde5143853ebd0e2465a462d5015d4b6a6bee
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-10-25 10:31:08 -0300

    Project created

---
+++ src/main/java/com/claudionogueira/springboot/LoginSpringBootSecurityThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.claudionogueira.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSpringBootSecurityThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSpringBootSecurityThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/claudionogueira/springboot/LoginSpringBootSecurityThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claudionogueira.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSpringBootSecurityThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
